e61e9bad8def82b41e8b8905761b5326ccba5d69
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 16:57:54 +0530

    Create vars.py

---
+++ wroxen/vars.py
@@ -0,0 +1,24 @@
+# (©) TheLx0980
+
+import os
+
+
+#Bot token @Botfather
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+#Your API ID from my.telegram.org
+
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+#Your API Hash from my.telegram.org
+
+API_HASH = os.environ.get("API_HASH", "")
+
+#Your db channel Id
+
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+
+#OWNER ID
+
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
