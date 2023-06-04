e146765b19b9a76b7c7332aaac8fd768112da69c
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 17:14:52 +0530

    Update vars.py

--- wroxen/vars.py
+++ wroxen/vars.py
@@ -14,3 +14,32 @@ API_HASH = os.environ.get("API_HASH", "")
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+class Wroxen(Client):
+    def __init__(self):
+        super().__init__(
+            name='wroxen_Bot',
+            api_id=APP_ID,
+            api_hash=API_HASH,
+            bot_token=TG_BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5
+        )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        logging.info(f"@{me.username} Is Started!")
+        try:
+            await self.send_message(LOG_CHANNEL, "Bot Restarted!")
+        except:
+            pass
+
+    async def stop(self, *args):
+        await super().stop()              
+        logging.info("Bot stopped. Bye.")
+        
+Wroxen().run()
