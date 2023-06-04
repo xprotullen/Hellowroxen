418857a6c5e9a66fc88f11a9e77e8d6aa6e9e8e8
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 21:18:40 +0530

    Update __init__.py

--- wroxen/__init__.py
+++ wroxen/__init__.py
@@ -1,1 +1,15 @@
 from .wroxen import Wroxen
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6018676334:AAGRzxeeL7cbPb2noJowezSiOyMC8sVlXzc")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", 5326801541))
+
+DB_NAME = "helloLx"
+DB_URL = os.environ.get("DB_URL", "")
