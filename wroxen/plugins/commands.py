ca92c05dc50e2b2122821014bbbab62ab8f69819
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 17:37:56 +0530

    Update commands.py

--- wroxen/plugins/commands.py
+++ wroxen/plugins/commands.py
@@ -3,11 +3,12 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters
 from wroxen import Wroxen
+from wroxen.text import ChatMSG
 
 @Wroxen.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
     await message.reply(
-        text=Translation.START,
+        text=ChatMSG.START_TXT.format(message.from_user.first_name),
         disable_web_page_preview=True,
         reply_markup = url_button,
         quote=True
@@ -17,7 +18,7 @@ async def start(client, message):
 @Wroxen.on_message(filters.command("about") & filters.private & filters.incoming)
 async def about(client, message):
     await message.reply(
-        text=Translation.ABOUT,
+        text=ChatMSG.ABOUT_TXT,
         disable_web_page_preview=True,
         reply_markup = url_button,
         quote=True
