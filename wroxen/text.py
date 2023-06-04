e7a2c11ecde0731f7dcd0fcd0725e266961688e6
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 17:31:25 +0530

    Update and rename __init__.py to text.py

---
+++ wroxen/text.py
@@ -0,0 +1,18 @@
+# (c) TheLx0980
+
+class CHATMSG(object):
+
+      # Start text
+      START_MSG = """
+नमस्ते {}
+      
+मेरा नाम रॉक्सन है, मेरे बारे में और जानने के लिए नीचे दिए हुए मदद बटन पर दबाए| 
+      """
+      
+      #help text
+      HELP_MSG = """
+help message 
+"""
+   
+    
+      
