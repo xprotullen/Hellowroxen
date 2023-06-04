95d0d488c6fab1393eeb9dfd39905a419972aea4
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 19:57:14 +0530

    Update text.py

--- wroxen/text.py
+++ wroxen/text.py
@@ -3,15 +3,17 @@
 class CHATMSG(object):
 
       # Start text
-      START_MSG = """
+      START_TXT = """
 नमस्ते {}
       
 मेरा नाम रॉक्सन है, मेरे बारे में और जानने के लिए नीचे दिए हुए मदद बटन पर दबाए| 
       """
       
       #help text
-      HELP_MSG = """
+      HELP_TXT = """
 help message 
+
+@lx0980AI
 """
    
     
