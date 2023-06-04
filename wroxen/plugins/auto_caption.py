62f8f4b1611200bd598001ee9d2f08009ef557d0
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-05 00:32:38 +0530

    Update auto_caption.py

--- wroxen/plugins/auto_caption.py
+++ wroxen/plugins/auto_caption.py
@@ -9,37 +9,20 @@ import logging
 media_filter = filters.document | filters.video | filters.audio
 logger = logging.getLogger(__name__)
 
-user_states = {}
-
-
 @Client.on_message(filters.command("set_caption"))
 async def set_caption_command(bot, message):
     user_id = message.from_user.id
-    user_states[user_id] = {"step": 0, "channel_id": None, "caption": None}
+    command_parts = message.text.split("::", 1)
 
-    await message.reply("Send your channel ID:")
-    user_states[user_id]["step"] = 1
-
-@Client.on_message(filters.private & ~filters.command)
-async def handle_private_message(bot, message):
-    user_id = message.from_user.id
-
-    if user_id not in user_states:
+    if len(command_parts) != 2:
+        await message.reply("Invalid format. Please use the format `/set_caption {channel_id}::{channel_caption}`.")
         return
 
-    state = user_states[user_id]
+    channel_id = command_parts[0].split()[1]
+    caption = command_parts[1]
 
-    if state["step"] == 1:
-        state["channel_id"] = message.text
-        await message.reply("Your channel ID has been recorded. Now send your channel caption:")
-        state["step"] = 2
-    elif state["step"] == 2:
-        state["caption"] = message.text
-        channel_id = state["channel_id"]
-        caption = state["caption"]
-        set_caption(user_id, channel_id, caption)
-        await message.reply(f"Your caption `{caption}` has been set for channel `{channel_id}`.")
-        del user_states[user_id]
+    set_caption(user_id, channel_id, caption)
+    await message.reply(f"Caption set for channel {channel_id}.")
 
     
 @Client.on_message(filters.command("delete_info"))
