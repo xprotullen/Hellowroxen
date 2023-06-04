b039b81f0b3a3d67820e61e9292cd5e6b96bfd4a
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 19:33:02 +0530

    Update auto_caption.py

--- wroxen/plugins/auto_caption.py
+++ wroxen/plugins/auto_caption.py
@@ -1,4 +1,6 @@
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import Client, filters, enums
+from wroxen import Wroxen
 
 
 @Client.on_message(filters.command("set_caption"))
@@ -53,3 +55,34 @@ def delete_caption(user_id, channel_id):
     result = captions_collection.delete_one({"user_id": user_id, "channel_id": channel_id})
     return result.deleted_count > 0
   
+@Client.on_message(filters.channel & (media_filter))
+async def editing(bot, message):
+    channel_id = message.chat.id
+    user_caption = get_caption(message.from_user.id, channel_id)
+
+    if user_caption:
+        try:
+            media = message.document or message.video or message.audio
+            caption_text = user_caption
+        except:
+            caption_text = ""
+            pass
+
+        if (message.document or message.video or message.audio):
+            if message.caption:
+                file_caption = f"**{message.caption}**"
+            else:
+                fname = media.file_name
+                filename = fname.replace("_", ".")
+                file_caption = f"`{filename}`"
+
+        try:
+            if caption_position == "top":
+                await bot.edit_message_caption(
+                    chat_id=message.chat.id,
+                    message_id=message.message_id,
+                    caption=file_caption + "\n" + caption_text,
+                    parse_mode=enums.ParseMode.MARKDOWN
+                )
+        except:
+            pass
