4dbd08c8402511053a15e79cc435cb16d1862054
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 21:21:06 +0530

    Update wroxen.py

--- wroxen/wroxen.py
+++ wroxen/wroxen.py
@@ -3,25 +3,7 @@
 import logging
 import logging.config
 import os
-
-
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6018676334:AAGRzxeeL7cbPb2noJowezSiOyMC8sVlXzc")
-
-#Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
-
-#Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
-
-#OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", 5326801541))
-
-DB_NAME = "helloLx"
-DB_URL = os.environ.get("DB_URL", "")
-
-
-
+from . import APP_ID, API_HASH, TG_BOT_TOKEN
 #-------------------------------------------------------------------------------------------------#
 from pyrogram import Client, __version__ 
 
