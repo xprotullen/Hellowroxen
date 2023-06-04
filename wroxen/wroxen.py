31e12e0c98f882315aec358f55b2eca3708655fb
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-04 21:31:13 +0530

    Update wroxen.py

--- wroxen/wroxen.py
+++ wroxen/wroxen.py
@@ -1,40 +1,36 @@
-# (©) TheLx0980
+# (©) TheLx0980 
 
-import logging
-import logging.config
-import os
-from . import APP_ID, API_HASH, TG_BOT_TOKEN
-#-------------------------------------------------------------------------------------------------#
-from pyrogram import Client, __version__ 
+from pyrogram import Client, enums, __version__
 
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+from . import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
 
 class Wroxen(Client):
+
     def __init__(self):
         super().__init__(
-            name='wroxen_Bot',
-            api_id=APP_ID,
+            "wroxen",
             api_hash=API_HASH,
-            bot_token=TG_BOT_TOKEN,
-            workers=50,
-            plugins={"root": "wroxen/plugins"},
-            sleep_threshold=5
+            api_id=APP_ID,
+            plugins={
+                "root": "wroxen/plugins"
+            },
+            workers=200,
+            bot_token=BOT_TOKEN,
+            sleep_threshold=10
         )
+        self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        me = await self.get_me()
-        logging.info(f"@{me.username} Is Started!")
-        try:
-            await self.send_message(OWNER_ID, "Bot Restarted!")
-        except:
-            pass
+        bot_details = await self.get_me()
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(
+            f"@{bot_details.username}  started! "
+        )
+       
 
     async def stop(self, *args):
-        await super().stop()              
-        logging.info("Bot stopped. Bye.")
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
         
 
