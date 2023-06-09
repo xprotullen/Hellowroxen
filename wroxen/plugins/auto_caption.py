fab900d63bcc22a688ec4dd3db39da8fc0a161f6
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-09 19:51:20 +0530

    Update auto_caption.py

--- wroxen/plugins/auto_caption.py
+++ wroxen/plugins/auto_caption.py
@@ -132,8 +132,8 @@ async def editing(bot, message):
         except:
             pass
     else:
-        forward_chat = message.chat.id
-        forward_settings = get_forward_settings(forward_chat)                                           
+        channel_id = message.chat.id
+        forward_settings = get_forward_settings(channel_id)                                           
         print(f"Forward settings: {forward_settings}")
         if forward_settings:
             from_chat = forward_settings["from_chat"]
