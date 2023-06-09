d8efeb05f399be22f781975fe7dc2719ef100a7d
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-09 14:46:55 +0530

    Create auto_forward.py

---
+++ wroxen/plugins/auto_forward.py
@@ -0,0 +1,46 @@
+# (c) @TheLx0980
+
+import logging
+from pyrogram import Client, filters, enums
+from wroxen.database.autoforward import set_forward_settings, get_forward_settings
+  
+logger = logging.getLogger(__name__)
+
+@Client.on_message(filters.command("AutoForward"))
+async def set_forward_command(bot, message):
+    command_parts = message.text.split(" ", 1)
+
+    if len(command_parts) != 2:
+        await message.reply("Invalid format. Please use the format `/AutoForward {to_chat}`.")
+        return
+
+    to_chat = command_parts[1]
+    if not to_chat.startswith("-100"):
+        to_chat = "-100" + to_chat
+
+    from_chat = message.chat.id
+
+    try:
+        set_forward_settings(from_chat, to_chat)
+        await message.reply("Auto forwarding settings updated.")
+    except:
+        await message.reply("Failed to update auto forwarding settings.")
+
+@Client.on_message(filters.channel & media_filter)
+async def forward(bot, update):
+    forward_settings = get_forward_settings()
+
+    if forward_settings:
+        from_chat = forward_settings["from_chat"]
+        to_chat = forward_settings["to_chat"]
+
+        try:
+            await bot.copy_message(
+                chat_id=from_chat,
+                from_chat_id=to_chat,
+                message_id=update.id,
+                caption=f"**{update.caption}**",
+                parse_mode=enums.ParseMode.MARKDOWN
+            )
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
