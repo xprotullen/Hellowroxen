1c307b9fe122d8cba4522a5b976bbacf0f618c09
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-10 02:45:32 +0530

    Update auto_forward.py

--- wroxen/plugins/auto_forward.py
+++ wroxen/plugins/auto_forward.py
@@ -4,7 +4,7 @@
 import logging
 from pyrogram import Client, filters, enums
 from wroxen.database.caption_db import set_forward_settings, delete_forward_settings, get_forward_settings, \
-   clear_forward_db, update_forward_settings, update_replace_text, update_f_caption, add_replace_settings, \
+   clear_forward_db, update_replace_text, update_f_caption, add_replace_settings, \
    delete_caption_settings, delete_replace_settings
    
 logger = logging.getLogger(__name__)
