7cfebeb084bed7f664fec54b3eee5bf4ef7573dd
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-10 13:49:03 +0530

    Update authorized_chat.py

--- wroxen/database/authorized_chat.py
+++ wroxen/database/authorized_chat.py
@@ -3,8 +3,20 @@
 from . import authorized_channels
 
 def get_authorized_channels():
-    """
-    Retrieve all authorized channels from the MongoDB database.
-    """
     channels = authorized_channels.find()
     return [channel["channel_id"] for channel in channels]
+
+def delete_authorized_channel(channel_id):
+    authorized_channels.delete_one({"channel_id": channel_id})
+
+def add_authorized_channel(channel_id):
+    authorized_channels.insert_one({"channel_id": channel_id})
+    
+def delete_all_authorized_chats()
+    authorized_channels.delete_many({})
+
+    
+    
+    
+    
+    
