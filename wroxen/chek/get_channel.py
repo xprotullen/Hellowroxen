0473f60a2b20696c3ab4f56cd32617fafbe4d438
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-12 12:49:24 +0530

    Update get_channel.py

--- wroxen/chek/get_channel.py
+++ wroxen/chek/get_channel.py
@@ -1,15 +1,16 @@
 # (c) @TheLx0980
 
-from wroxen.database import channels_collection, get_caption
+from wroxen.database import Database 
+db = Database ()
 
 def get_channel_info(user_id):
-    channel_doc = channels_collection.find_one({"user_id": user_id})
+    channel_doc = db.channels_collection.find_one({"user_id": user_id})
     if channel_doc:
         channel_id = channel_doc["channel_id"]
-        caption = get_caption(user_id, channel_id)
+        caption = db.get_caption(user_id, channel_id)
         return channel_id, caption
     return None, None
 
 def is_channel_added(channel_id):
-    channel_doc = channels_collection.find_one({"channel_id": channel_id})
+    channel_doc = db.channels_collection.find_one({"channel_id": channel_id})
     return channel_doc is not None
