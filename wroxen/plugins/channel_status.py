31f8ff7078487126678f7fc25016bc89d303ae66
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-12 13:57:55 +0530

    Update channel_status.py

--- wroxen/plugins/channel_status.py
+++ wroxen/plugins/channel_status.py
@@ -9,7 +9,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 db = Database()
-auth = AuthorizedChannels()
 
 @Client.on_message(filters.command("Channel_status") & filters.channel)
 async def channel_status_command(bot, message):
