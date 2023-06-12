8c16e7d60db038611185e3b0ca3ef337726f5a67
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-12 15:43:18 +0530

    Update authorized_chat.py

--- wroxen/database/authorized_chat.py
+++ wroxen/database/authorized_chat.py
@@ -6,20 +6,25 @@ db = Database()
 Auth = db.authorized_channels
 
 class AuthorizedChannels:
+    @staticmethod
     def get_authorized_channels(channel_id):
         channels = Auth.find({"channel_id": channel_id})
         return [channel["channel_id"] for channel in channels]
     
+    @staticmethod  
     def delete_authorized_channel(channel_id):
         Auth.delete_one({"channel_id": channel_id})
     
+    @staticmethod
     def add_authorized_channel(channel_id):
         Auth.insert_one({"channel_id": channel_id})
     
+    @staticmethod
     def delete_all_authorized_chats():
         result = Auth.delete_many({})
         return result.deleted_count
     
+    @staticmethod
     def get_authorized_chat():
         channels = Auth.find({})
         return [channel["channel_id"] for channel in channels]
