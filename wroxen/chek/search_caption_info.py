fdaccad90cef67a8532b44d7c811847ab600ddcb
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-14 16:58:51 +0530

    Create search_caption_info.py

---
+++ wroxen/chek/search_caption_info.py
@@ -0,0 +1,55 @@
+# (c) @TheLx0980
+
+import re
+
+def extract_movie_details(caption):
+    # Movie name patterns
+    name_patterns = [
+        r"^([^.]*?)\s\d{4}",  # Pattern 1: Movie name followed by year (excluding ".")
+        r"^([^.]*?)\s(\(\d{4}\))",  # Pattern 2: Movie name followed by (year) (excluding ".")
+        r"^([^.]*?)\s\d{3,4}p",  # Pattern 3: Movie name followed by quality (excluding ".")
+        r"^([^.]*?)(?=\s\d{4})",  # Pattern 20: Movie name until a space followed by 4 digits (year) (excluding ".")
+    ]
+    
+    # Year patterns
+    year_patterns = [
+        r"\((\d{4})\)",  # Pattern 1: (year)
+        r"(\d{4})",  # Pattern 2: year
+        r"\d{2}(\d{2})\b",  # Pattern 3: last 2 digits of the year
+        r"(\d{4})\b",  # Pattern 4: year
+        r"(19\d{2}|20\d{2})",  # Pattern 5: year between 1900 and 2099
+        r"(\d{2})(\d{2})\b",  # Pattern 6: 4-digit year split into two groups of 2 digits.
+        r"\b(20[012]\d|19[5-9]\d)\b",  # Pattern 20: year between 1950 and 2099
+        r"Movie :-\s(.*?)\(\d{4}\)"
+    ]
+    
+    # Quality patterns
+    quality_patterns = [
+        r"\d{3,4}p",  # Pattern 1: 3 or 4 digits followed by 'p'
+        r"\b(4|8|10)K\b",  # Pattern 2: 4K, 8K, 10K
+        r"\b(FHD|HD|SD)\b",  # Pattern 3: FHD, HD, SD
+    ]
+
+    movie_name = "Unknown"
+    year = "Unknown"
+    quality = "Unknown"
+
+    for name_pattern in name_patterns:
+        movie_name_match = re.search(name_pattern, caption)
+        if movie_name_match:
+            movie_name = movie_name_match.group(1).replace(".", "")
+            break
+
+    for year_pattern in year_patterns:
+        year_match = re.search(year_pattern, caption)
+        if year_match:
+            year = year_match.group(1)
+            break
+
+    for quality_pattern in quality_patterns:
+        quality_match = re.search(quality_pattern, caption)
+        if quality_match:
+            quality = quality_match.group()
+            break
+
+    return movie_name, year, quality
