ccab775ad2067915af404edb9c31f486d1f98ec3
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-19 17:21:06 +0530

    Update search_caption_info.py

--- wroxen/chek/search_caption_info.py
+++ wroxen/chek/search_caption_info.py
@@ -6,49 +6,6 @@ from pyrogram import enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from html import escape
 
-DATABASE = {}
-
-async def send_result_message(client, message, query, movies, page, result_message_id=None):
-    total_results = len(movies)
-
-    if total_results <= 10:
-        # Less than or equal to 10 results, no need for pagination
-        movies_page = movies
-        reply_markup = None
-    else:
-        results_per_page = 10
-        start_index = (page - 1) * results_per_page
-        end_index = page * results_per_page
-        movies_page = movies[start_index:end_index]
-        reply_markup = generate_inline_keyboard(query, total_results, page)
-
-    result_message = generate_result_message(query, movies_page, page)
-
-    if result_message_id:
-        # Edit the existing message
-        await client.edit_message_text(
-            chat_id=message.chat.id,
-            message_id=result_message_id,
-            text=result_message,
-            parse_mode=enums.ParseMode.HTML,
-            reply_markup=reply_markup
-        )
-    else:
-        # Send a new message
-        sent_message = await message.reply_text(
-            result_message,
-            parse_mode=enums.ParseMode.HTML,
-            reply_markup=reply_markup
-        )
-        result_message_id = sent_message.id
-
-    DATABASE[query] = {
-        'message_id': result_message_id,
-        'movies': movies,
-        'page': page
-    }
-
-
 def generate_inline_keyboard(query, total_results, current_page):
     buttons = []
 
