e8c5795299e17ff77059696643f639f6671fe0f8
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-19 21:03:35 +0530

    Update search_msg.py

--- wroxen/plugins/search_msg.py
+++ wroxen/plugins/search_msg.py
@@ -1,21 +1,89 @@
 # (c) @TheLx0980
 # Year : 2023
 
-import re, pyrogram
+import re
 from pyrogram import filters, enums, Client
 from wroxen.wroxen import Wroxen as Bot
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import logging
-from wroxen.database.search_msg_db import get_channel_id
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+from wroxen.database.search_msg_db import get_channel_id, add_channel, delete_channel, is_group_in_database
+from wroxen.chek.search_caption_info import send_result_message, extract_movie_details
 
 MEDIA_FILTER = enums.MessagesFilter.VIDEO 
-BUTTONS = {}
-DATABASE = {}
 
 from html import escape
+
+
+@Client.on_message(filters.command("add_search_cnl") & filters.reply)
+async def add_channel_handler(client, message):
+    chat_id = message.chat.id
+    group_id = message.chat.id
+    channel_id = message.reply_to_message.forward_from_chat.id
+
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"आप एक अज्ञात व्यवस्थापक हैं।")
+
+    chat_type = message.chat.type
+    if chat_type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        return
+
+    st = await client.get_chat_member(chat_id, userid)
+    if (
+        st.status not in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER]
+    ):
+        return
+
+    # जांचें कि समूह ID पहले से ही डेटाबेस में मौजूद है
+    if is_group_in_database(group_id):
+        # अगर चैनल ID पहले से मौजूद है, तो एक त्रुटि संदेश भेजें
+        if get_channel_id(group_id) == channel_id:
+            await client.send_message(
+                chat_id=chat_id,
+                text="समूह के लिए चैनल आईडी पहले से ही जोड़ दिया गया है।"
+            )
+        else:
+            add_channel(group_id, channel_id)
+            await client.send_message(
+                chat_id=chat_id,
+                text="समूह के लिए चैनल आईडी अपडेट की गई है।"
+            )
+    else:
+        add_channel(group_id, channel_id)
+        await client.send_message(
+            chat_id=chat_id,
+            text="चैनल आईडी को डेटाबेस में जोड़ दिया गया है।"
+        )
+
+@Client.on_message(filters.command("delete_search_cnl") & filters.group)
+async def delete_channel_handler(client, message):
+    chat_id = message.chat.id
+    group_id = message.chat.id
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"आप एक अज्ञात व्यवस्थापक हैं।")
+
+    chat_type = message.chat.type
+    if chat_type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        return
+
+    st = await client.get_chat_member(chat_id, userid)
+    if (
+        st.status not in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER]
+    ):
+        return
+        
+    if is_group_in_database(group_id):
+        delete_channel(group_id)
+        await client.send_message(
+            chat_id=chat_id,
+            text="समूह और उससे संबंधित चैनल को डेटाबेस से हटा दिया गया है।"
+        )
+    else:
+        await client.send_message(
+            chat_id=chat_id,
+            text="समूह डेटाबेस में मौजूद नहीं है।"
+        )
 
 
 
@@ -35,9 +103,10 @@ async def filter(client: Client, message: Message):
         msgs = []
         search_id = int(channel_id)
         async for msg in client.USER.search_messages(search_id, query=query, filter=MEDIA_FILTER):
-            file_name = msg.caption
+            caption = msg.caption
             link = msg.link
-            movie_text = f"<b>{file_name}</b>\nLink: {link}"
+            movie_name, year, quality = extract_movie_details(caption)
+            movie_text = f"<b>{escape(movie_name)} ({year}) {quality}</b>\n<b>Link:</b> {link}"
             msgs.append(movie_text)
 
         if not msgs:
@@ -47,76 +116,6 @@ async def filter(client: Client, message: Message):
         await send_result_message(client, message, query, msgs, page)
 
 
-async def send_result_message(client, message, query, movies, page, result_message_id=None):
-    total_results = len(movies)
 
-    if total_results <= 10:
-        # Less than or equal to 10 results, no need for pagination
-        movies_page = movies
-        reply_markup = None
-    else:
-        results_per_page = 10
-        start_index = (page - 1) * results_per_page
-        end_index = page * results_per_page
-        movies_page = movies[start_index:end_index]
-        reply_markup = generate_inline_keyboard(query, total_results, page)
-
-    result_message = generate_result_message(query, movies_page, page)
-
-    if result_message_id:
-        # Edit the existing message
-        await client.edit_message_text(
-            chat_id=message.chat.id,
-            message_id=result_message_id,
-            text=result_message,
-            parse_mode=enums.ParseMode.HTML,
-            reply_markup=reply_markup
-        )
-    else:
-        # Send a new message
-        sent_message = await message.reply_text(
-            result_message,
-            parse_mode=enums.ParseMode.HTML,
-            reply_markup=reply_markup
-        )
-        result_message_id = sent_message.id
-
-    DATABASE[query] = {
-        'message_id': result_message_id,
-        'movies': movies,
-        'page': page
-    }
-
-
-
-
-
-def generate_inline_keyboard(query, total_results, current_page):
-    buttons = []
-
-    if total_results > current_page * 10:
-        next_page_button = InlineKeyboardButton(
-            text='Next Page',
-            callback_data=f'next_page:{query}:{current_page + 1}'
-        )
-        buttons.append(next_page_button)
-
-    if current_page > 1:
-        previous_page_button = InlineKeyboardButton(
-            text='Previous Page',
-            callback_data=f'previous_page:{query}:{current_page - 1}'
-        )
-        buttons.append(previous_page_button)
-
-    inline_keyboard = [buttons]
-    return InlineKeyboardMarkup(inline_keyboard) 
-
-
-def generate_result_message(query, movies, page):
-    start_number = (page - 1) * 10 + 1
-    result_message = f"Here are the results for <b>{escape(query)}</b>:\n\n"
-    for i, movie_text in enumerate(movies, start=start_number):
-        result_message += f"{i}. {movie_text}\n\n"
-    return result_message
 
 
