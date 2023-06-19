81658d5878c0449aa37051fa61153fc50542c50a
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-19 19:58:32 +0530

    Update commands.py

--- wroxen/plugins/commands.py
+++ wroxen/plugins/commands.py
@@ -5,6 +5,7 @@ from pyrogram import filters, Client, enums
 from wroxen.wroxen import Wroxen
 from wroxen.text import ChatMSG
 from wroxen.vars import ADMIN_IDS
+from .search_msg import DATABASE 
 
 import logging
 logger = logging.getLogger(__name__)
@@ -45,7 +46,7 @@ async def about(client, message):
     )
 
 
-@Client.on_callback_query()
+HELLO = """ @Client.on_callback_query()
 async def callback_data(bot, update: CallbackQuery):
 
     query_data = update.data
@@ -185,3 +186,172 @@ async def callback_data(bot, update: CallbackQuery):
 
     elif query_data == "close":
         await update.message.delete()
+"""
+
+@Client.on_callback_query()
+async def callback_handler(client: Client, query: CallbackQuery):
+    data = query.data
+    
+    if data.startswith('next_page:'):
+        _, query_text, page = data.split(':')
+        
+        # Retrieve data from DATABASE
+        db_entry = DATABASE.get(query_text)
+        if db_entry:
+            movies = db_entry['movies']
+            result_message_id = db_entry['message_id']
+            
+            await query.answer()
+            await send_result_message(client, query.message, query_text, movies, int(page), result_message_id)
+        else:
+            await query.answer("Thats not for you!!", show_alert=True)
+    
+    elif data.startswith('previous_page:'):
+        _, query_text, page = data.split(':')
+        
+        # Retrieve data from DATABASE
+        db_entry = DATABASE.get(query_text)
+        if db_entry:
+            movies = db_entry['movies']
+            result_message_id = db_entry['message_id']
+            
+            await query.answer()
+            await send_result_message(client, query.message, query_text, movies, int(page), result_message_id)
+        else:
+            await query.answer("Thats not for you!!", show_alert=True)
+    
+    elif data == "start":
+        buttons = [[            
+            InlineKeyboardButton('मदद ⚙', callback_data="help"),
+            InlineKeyboardButton("🔒 बंद करो", callback_data="close")
+        ]]
+    
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.START_TXT.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML,
+            disable_web_page_preview=True
+        )
+
+    elif data == "help":
+        buttons = [[
+            InlineKeyboardButton('पीछे जाए ⚡', callback_data='start'),
+            InlineKeyboardButton('मेरे बारे में', callback_data='about')
+        ], [
+            InlineKeyboardButton('स्वचालित कैप्शन', callback_data='caption')
+        ], [
+            InlineKeyboardButton('स्वचालित फ़ॉरवर्ड', callback_data='autoforward')
+        ], [
+            InlineKeyboardButton('मीडिया क्लोन', callback_data='media_clone')
+        ], [
+            InlineKeyboardButton('व्यवस्थापक आदेश', callback_data='admin_command')
+        ], [ 
+            InlineKeyboardButton('चैनल में खोजें', callback_data='channel_search')
+        ], [
+            InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+    
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.HELP_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML,
+            disable_web_page_preview=True
+        )
+
+    elif data == "about": 
+        buttons = [[
+            InlineKeyboardButton('पीछे ⚡', callback_data='start'),
+                        InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.ABOUT_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+    
+    elif data == "caption": 
+        buttons = [[
+            InlineKeyboardButton('पीछे⚡', callback_data='help'),
+            InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.CAPTION_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+  
+    elif data == "autoforward": 
+        buttons = [[
+            InlineKeyboardButton('पीछे⚡', callback_data='help'),
+            InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.AUTO_FORWARD_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+        
+    elif data == "admin_command": 
+        user_id = query.from_user.id
+        if user_id not in ADMIN_IDS:            
+            await query.answer("आप बोट व्यवस्थापक नहीं है।", show_alert=True)
+            return
+        buttons = [[
+            InlineKeyboardButton('पीछे⚡', callback_data='help'),
+            InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.ADMIN_COMMAND_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+        
+    elif data == "media_clone": 
+        buttons = [[
+            InlineKeyboardButton('पीछे⚡', callback_data='help'),
+            InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.MEDIA_CLONE_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    elif data == "channel_search": 
+        buttons = [[
+            InlineKeyboardButton('पीछे⚡', callback_data='help'),
+            InlineKeyboardButton('बंद करें 🔐', callback_data='close')
+        ]]
+        
+        reply_markup = InlineKeyboardMarkup(buttons)
+        
+        await query.message.edit_text(
+            ChatMSG.CHANNEL_SEARCH_TXT,  
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    elif data == "close":
+        await query.message.delete()
+
+
+
