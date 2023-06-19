9ca2370884a871ab57a95843c36a2dff30642ef7
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-19 15:07:59 +0530

    Update search_msg_db.py

--- wroxen/database/search_msg_db.py
+++ wroxen/database/search_msg_db.py
@@ -1,27 +1,26 @@
 # (c) @TheLx0980
+# Year: 2023 
 
-from wroxen.database import db as DB
+from . import Database
+db = Database()
 
-group_search_collection = DB["group_search"]
 
-def add_group_search(group_id, search_channel_id):
-    group_search_data = {"group_id": group_id, "search_channel_id": search_channel_id}
-    group_search_collection.insert_one(group_search_data)
+def add_channel(group_id, channel_id):
+    query = {"group_id": group_id}
+    update = {"$set": {"channel_id": channel_id}}
+    collection.update_one(query, update, upsert=True)
 
-def get_search_channel_id(group_id):
-    group_search_data = group_search_collection.find_one({"group_id": group_id})
-    if group_search_data:
-        return group_search_data["search_channel_id"]
-    return None
-  
- def update_group_search(group_id, search_channel_id):
-    group_search_collection.update_one(
-        {"group_id": group_id},
-        {"$set": {"search_channel_id": search_channel_id}}
-    ) 
-  
-def delete_group_search(group_id):
-    group_search_collection.delete_one({"group_id": group_id}) 
-  
-  
-  
+
+def is_group_in_database(group_id):
+    query = {"group_id": group_id}
+    result = collection.find_one(query)
+    return result is not None
+
+def get_channel_id(group_id):
+    query = {"group_id": group_id}
+    result = collection.find_one(query)
+    return result["channel_id"] if result else None
+
+def delete_channel(group_id):
+    query = {"group_id": group_id}
+    collection.delete_one(query)
