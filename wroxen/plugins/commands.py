d1f0955308af9b47100cf86e0d43fe3d5134f298
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-19 23:06:47 +0530

    Update commands.py

--- wroxen/plugins/commands.py
+++ wroxen/plugins/commands.py
@@ -200,34 +200,37 @@ async def callback_data(bot, update: CallbackQuery):
 @Client.on_callback_query()
 async def callback_handler(client: Client, query: CallbackQuery):
     data = query.data
-    
     if data.startswith('next_page:'):
-        _, query_text, page = data.split(':')
+        if query.message.reply_to_message.from_user.id == query.from_user.id:
+            _, query_text, page = data.split(':')
         
-        # Retrieve data from DATABASE
-        db_entry = DATABASE.get(query_text)
-        if db_entry:
-            movies = db_entry['movies']
-            result_message_id = db_entry['message_id']
+            # Retrieve data from DATABASE
+            db_entry = DATABASE.get(query_text)
+            if db_entry:
+                movies = db_entry['movies']
+                result_message_id = db_entry['message_id']
             
-            await query.answer()
-            await send_result_message(client, query.message, query_text, movies, int(page), result_message_id)
+                await query.answer()
+                await send_result_message(client, query.message, query_text, movies, int(page), result_message_id)
+            else:
+                await query.answer("यह मैसेज काफी पुराना हो चुका है।")
         else:
-            await query.answer("Thats not for you!!", show_alert=True)
-    
+            await query.answer("यह आपके लिए नही है!", show_alert=True)
+   
     elif data.startswith('previous_page:'):
-        _, query_text, page = data.split(':')
+        if query.message.reply_to_message.from_user.id == query.from_user.id:
+            _, query_text, page = data.split(':')
         
-        # Retrieve data from DATABASE
-        db_entry = DATABASE.get(query_text)
-        if db_entry:
-            movies = db_entry['movies']
-            result_message_id = db_entry['message_id']
-            
-            await query.answer()
-            await send_result_message(client, query.message, query_text, movies, int(page), result_message_id)
+            # Retrieve data from DATABASE
+            db_entry = DATABASE.get(query_text)
+            if db_entry:
+                movies = db_entry['movies']
+                result_message_id = db_entry['message_id']
+                await send_result_message(client, query.message, query_text, movies, int(page), result_message_id)
+            else:
+                await query.answer("यह मैसेज काफी पुराना हो चुका है।")
         else:
-            await query.answer("Thats not for you!!", show_alert=True)
+            await query.answer("यह आपके लिए नही है!", show_alert=True)
     
     elif data == "start":
         buttons = [[            
